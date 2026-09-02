5a800623bbeaa9ec958ffb99aad0fc4e6e3dccd5
veeramanoharb-ai <veeramanoharb@gmail.com>
2026-09-02 16:46:32 +0530

    Create Day_01/day_01.py

---
+++ Day_01/day_01.py
@@ -0,0 +1,6 @@
+# Day 1 - 30 Days of Python
+print("Hello, World!")
+print("Welcome to 30 Days of Python Challenge")
+print("Name: Veera Manohar B")
+print("Branch: CSE - Section A")
+print("University: MSRUAS")
