b16cdd8e573d74c8a581a3476f87ca9c956d2f29
veeramanoharb-ai <veeramanoharb@gmail.com>
2026-09-03 20:46:39 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,61 @@
+first_name = "veera"
+last_name = "manohar"
+full_name = "veera manohar"
+country = "india"
+city = "mumbai"
+age = 18
+year = 2008
+is_married = False
+is_true = True
+is_light_on = True
+x,y,z = 5,10,15
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(len(first_name))
+print(len(first_name))
+print(len(last_name))
+print(len(first_name) == len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff)
+product = num_one * num_two
+print(product)
+division = num_one / num_two
+print(division)
+remainder = num_two % num_one
+print(remainder)
+exp = num_one ** num_two
+print(exp)
+floor_division = num_one // num_two
+print(floor_division)
+radius = 30
+pi = 3.14
+area_of_circle = pi * radius ** 2
+circum_of_circle = 2 * pi * radius
+print(area_of_circle)
+print(circum_of_circle)
+radius = float(input("enter radius"))
+area = pi * radius ** 2
+print(area)
+first_name = input("enter your name")
+last_name = input("enter your last name")
+country = input("enter your country")
+age = input("enter your age")
+print(first_name)
+print(last_name)
+print(country)
+print(age)
+import keyword
+print(keyword.kwlist)
+help('keywords')
